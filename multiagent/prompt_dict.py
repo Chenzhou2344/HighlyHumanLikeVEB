9d4b88f803b44284311b81cecd7a3e6c00f2f140
HuiHan <104563162+abigcatcat@users.noreply.github.com>
2024-10-23 16:56:38 +0800

    Update prompt_dict.py

--- multiagent/prompt_dict.py
+++ multiagent/prompt_dict.py
@@ -1,12 +1,14 @@
 from prompt.overall import *
 from prompt.color import *
+from prompt.motion_effects import *
 # from prompt.misogynistic import *
 # from prompt.offensive import *
 # from prompt.sarcastic import *
 
 prompt = {
     "overall_consistency": overall_prompt,
-    "color": color_prompt
+    "color": color_prompt,
+    "motion_effects": motion_effects_prompt
 
 #     "hatefulness": hateful_prompt,
 #
