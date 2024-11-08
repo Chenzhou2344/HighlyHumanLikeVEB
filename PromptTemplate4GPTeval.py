5c11169bc697143011c29b378f361c80126bb10e
Chenzhou2344 <siyuan-l@sjtu.edu.cn>
2024-11-08 16:20:22 +0800

    11/8 7fewshot imaging

--- PromptTemplate4GPTeval.py
+++ PromptTemplate4GPTeval.py
@@ -332,7 +332,6 @@ Prompt4ImagingQuality = ["""
         ### Task Description:
         You are now an Video Evaluation Expert in evaluating generated videos and need to evaluate the videos from different models.
         During the evaluation, you must strictly adhere to 'Evaluation Criteria'.and 'Evaluation Steps'.
-        Each video should be evaluated independently, without comparisons to others.
 
         ### Evaluation Criteria:
         You will evaluate the image quality of each video, focusing on low-level distortions present in the frames. 
@@ -342,7 +341,6 @@ Prompt4ImagingQuality = ["""
         3. **Brightness**: Evaluate whether the brightness is reasonable and determine the extent of any overexposure.
 
    
-        
         ### Scoring Range:
         Assign a score from 1 to 5 for each video, with 5 representing the highest quality, based on the 'Evaluation Criteria':
         1. **Very Poor Quality (score=1)**: Significant distortion, severe blurriness, numerous noise spots, excessive brightness, resulting in a very poor viewing experience.
@@ -356,19 +354,7 @@ Prompt4ImagingQuality = ["""
         The frames from the video to be evaluated are provided after the example frames. 
         You should evaluate all input frames but just need to provide the score for the video to be evaluated. 
 
-        ### The Output Format:
-        Finally for the evaluation results, you should assign a score to each video and provide the reason behind the scores.
-        Assuming there are 1 video input from model 'A' scoring 'x',the format is:
-        <output format>
-        Final Scores:
-        - A: x ,because ...
-        </output format>
-            
-        <example>
-        Assuming there are 1 video input from model 'gen2' scoring '3',the format is:
-        Final Scores:
-        - gen2: 3 , because ...
-        </example>
+
 
 """
 ]
